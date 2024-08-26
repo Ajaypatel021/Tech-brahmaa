1f3015aafb388ce534a90e4c71ac20fe8ef6243d
Ajaypatel021 <forworkajay@gmail.com>
2024-08-26 15:35:54 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.querySelector('.menu-button');
+    const menu = document.querySelector('.menu');
+
+    menuButton.addEventListener('click', function () {
+        menu.classList.toggle('active');
+    });
+
+    // Additional JavaScript animations can be added here
+
+
+
+
+});
